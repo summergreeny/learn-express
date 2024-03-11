71211acdee062e85af923b9fa2129323be70a00f
yilinliu <yilin.liu@hotmail.com>
2024-03-11 17:01:56 -0400

    quiz

--- quiz/server.js
+++ quiz/server.js
@@ -1,56 +1,75 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 const port = 8000;
 
 let users;
-fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
-  console.log('reading file ... ');
-  if(err) throw err;
-  users = JSON.parse(data);
-})
+fs.readFile(
+  path.resolve(__dirname, "../data/users.json"),
+  function (err, data) {
+    console.log("reading file ... ");
+    if (err) throw err;
+    users = JSON.parse(data);
+  }
+);
 
 const addMsgToRequest = function (req, res, next) {
-  if(users) {
+  if (users) {
     req.users = users;
     next();
-  }
-  else {
+  } else {
     return res.json({
-        error: {message: 'users not found', status: 404}
+      error: { message: "users not found", status: 404 },
     });
   }
-  
-}
+};
 
-app.use(
-  cors({origin: 'http://localhost:3000'})
-);
-app.use('/read/usernames', addMsgToRequest);
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use("/read/usernames", addMsgToRequest);
 
-app.get('/read/usernames', (req, res) => {
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
+app.get("/read/usernames", (req, res) => {
+  let usernames = req.users.map(function (user) {
+    return { id: user.id, username: user.username };
   });
   res.send(usernames);
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/write/adduser', addMsgToRequest);
+app.use("/write/adduser", addMsgToRequest);
 
-app.post('/write/adduser', (req, res) => {
+app.post("/write/adduser", (req, res) => {
   let newuser = req.body;
   req.users.push(newuser);
-  fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
-    if (err) console.log('Failed to write');
-    else console.log('User Saved');
+  fs.writeFile(
+    path.resolve(__dirname, "../data/users.json"),
+    JSON.stringify(req.users),
+    (err) => {
+      if (err) console.log("Failed to write");
+      else console.log("User Saved");
+    }
+  );
+  res.send("done");
+});
+
+app.use("/read/username", addMsgToRequest);
+app.get("/read/username/:name", (req, res) => {
+  let name = req.params.name;
+  let users_with_name = req.users.filter(function (user) {
+    return user.username === name;
   });
-  res.send('done');
-})
+  console.log(users_with_name);
+  if (users_with_name.length === 0) {
+    res.send({
+      error: { message: `${name} not found`, status: 404 },
+    });
+  } else {
+    res.send(users_with_name);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
